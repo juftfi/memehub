dea1de6774198562363c5656349d860200b6329e
juftfi <107974770+juftfi@users.noreply.github.com>
2026-07-24 20:47:57 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -305,7 +305,7 @@ function Index() {
                   </div>
                   <div className="md:ml-auto flex flex-col sm:flex-row gap-2">
                     <a
-                      href="https://www.flap.sh"
+                      href="https://www.four.meme"
                       target="_blank"
                       rel="noreferrer"
                       className="rounded-md bg-[color:var(--buy)] text-primary-foreground px-5 py-2.5 font-medium whitespace-nowrap text-center"
